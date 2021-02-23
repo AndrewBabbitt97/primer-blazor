1f040bfd5585a07f9d77a5055807eda334fb2742
Andrew Babbitt <andrew.babbitt@recro.com>
2021-02-22 23:53:07 -0700

    Add documentation site

---
+++ src/Primer.Documentation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Primer.Documentation
+{
+    /// <summary>
+    /// The main program class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The program entry point
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
+        /// <returns>A task</returns>
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
